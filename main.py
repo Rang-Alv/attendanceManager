b0279154b022840365fc5f61b4597ac7818d561f
RangAl <71291523+Rang-Alv@users.noreply.github.com>
2022-06-15 15:34:53 +0100

    Updated teacher handling
    Updated teacher handling so that the teacher dict contains class names as well. When taking register the user enters their teacher ID and is shown the class they are taking the attendance of

--- main.py
+++ main.py
@@ -20,13 +20,15 @@ class Student:
 
 
 class Section:
-    def __init__(self, name, teacher_id=0):
+    def __init__(self, name, teacher_id=0, teacher_name=""):
         self.class_name = name
         self.teacher = teacher_id  # CHANGE to integer ID system
+        self.teacher_name = teacher_name
         self.students = {}  # dict containing attendance record
 
-    def add_teacher(self, teacher_id):
+    def add_teacher(self, teacher_id, teacher_name):
         self.teacher = teacher_id
+        self.teacher = teacher_name
 
     def add_student(self, student_id, attendance=None):
         if attendance is None:
@@ -38,12 +40,13 @@ class Section:
             input_atten = []
             for i in range(0, 8):
                 while True:
-                    print("Attendance for student:", id, " in week:", i)
+                    print("Attendance for student:", id, " in week:", i + 1)
                     week_atten = int(input())
                     if week_atten < 0 or week_atten > 5:
                         print("Attendance for the week can not be less than 0 or greater than 5")
                     else:
                         input_atten.append(week_atten)
+                        break
 
             self.students[id] = input_atten  # Set attendance
 
@@ -54,7 +57,8 @@ class Section:
         return self.class_name
 
     def display_details(self):
-        print("Class name:", self.class_name, " Teacher:", self.teacher, " Students:", self.students)
+        print("Class name:", self.class_name, " Teacher:", self.teacher_name, "", self.teacher, " Students:",
+              self.students)
 
 
 class User:
@@ -85,9 +89,10 @@ class User:
     # Method of writing student attendance
     def take_attendance(self):
         while True:
-            input_class = input("Enter section to take attendance:") #FIX
+            input_id = int(input("Enter teacher ID:"))
             try:
-                print(input_class[0], input_class[1])
+                input_class = self.teachers[input_id][1]
+                print("You are: ", self.teachers[input_id][0], "For class:", input_class[0], input_class[1])
                 selected_class = self.classes[int(input_class[0])]
                 selected_sect = selected_class[0]
                 for c in selected_class:
@@ -108,46 +113,45 @@ class User:
                 student_current = self.students[sid]
                 search_class = student_current.class_name
                 selected_class = self.classes[int(search_class[0])]
-                #selected_sect = selected_class[0]
+                # selected_sect = selected_class[0]
                 for c in selected_class:
                     if c.class_name == search_class:
                         selected_sect = c
                         break
                 # Get attendance record, calculate attendance percentages and update in student object
-                print(selected_sect.class_name)
+                # print(selected_sect.class_name)
                 # Get the attendance record from the section object and set the attendance record in the student object
                 student_current.record = selected_sect.students[sid]
             except(RuntimeError, TypeError, NameError, IndexError):
                 print("wrong")
 
-
     def display_attendance(self, student_id):
         try:
-            att_week, att_month, att_year = self.students[student_id].get_attendance()
+            # Get attendance by searching the dict for student ID
+            student_ob = self.students[student_id]
+            student_name = student_ob.name
+            student_class = student_ob.class_name
+            att_week, att_month, att_year = student_ob.get_attendance()
+            print("Attendance information for ", student_name, "from class", student_class, ":")
             print("Weekly:", att_week, "Monthly:", att_month, "Annual:", att_year)
         except KeyError:
             print("ERROR: That student does not exist")
 
 
-
-
 class Admin(User):
     def __init__(self):
-        teacher_list = [[1, "Mr. Macdonald"], [2, "Ms. Rolex"], [3, "Mr. Airbus"], [4, "Ms. Amadeus"], [5, "Mr. Bose"],
-                        [6, "Ms. Wiley"]]
-        list(filter(lambda teacher_info: self.add_teachers(teacher_info[0], teacher_info[1]), teacher_list))
-
         sect_list = [[1, "A"], [1, "B"], [1, "C"], [2, "A"], [3, "A"], [3, "B"]]
         list(filter(lambda sect_info: self.add_sect(sect_info[0], sect_info[1]), sect_list))
 
-        teacher_number = 1
-        for c in super().classes:
-            for n in super().classes[c]:
-                n.add_teacher(teacher_number)
-                teacher_number = teacher_number + 1
+        teacher_list = [[1, "Mr. Macdonald", "1A"], [2, "Ms. Rolex", "1B"], [3, "Mr. Airbus", "1C"],
+                        [4, "Ms. Amadeus", "2A"], [5, "Mr. Bose", "3A"],
+                        [6, "Ms. Wiley", "3B"]]
+        list(filter(lambda teacher_info: self.add_teachers(teacher_info[0], teacher_info[1], teacher_info[2]),
+                    teacher_list))
 
         stud_list = [["Toaster", 1, 6], ["Biscuit", 1, 7], ["Book", 1, 8], ["Radiator", 1, 9], ["Webcam", 1, 10],
-                     ["Clock", 1, 11], ["Toilet", 2, 12]]
+                     ["Clock", 1, 11], ["Toilet", 2, 12], ["Table", 3, 14], ["Grass", 3, 35], ["Foot", 3, 67],
+                     ["Xylophone", 3, 34]]
         list(filter(lambda stud_info: self.add_students(stud_info[0], stud_info[1], stud_info[2]), stud_list))
         super().update_attendance()
 
@@ -163,17 +167,26 @@ class Admin(User):
                 c.add_student(id)
                 break
             else:
-                print("No space")  # CHANGE make new section in class
+                print("")  # CHANGE make new section in class
+        super().update_attendance()
 
-    def add_teachers(self, id, name):
-        super().teachers[id] = name
+    def add_teachers(self, id, name, class_name):
+        super().teachers[id] = [name, class_name]
+        selected_class = super().classes[int(class_name[0])]
+        for c in selected_class:
+            if c.class_name == class_name:
+                selected_sect = c
+                break
+        # set the teacher's name in the class
+        selected_sect.teacher = id
+        selected_sect.teacher_name = name
 
     def add_sect(self, class_no, sect_name):
         s = Section(str(class_no) + sect_name)
         super().classes[class_no].append(s)
 
     def assign_teacher(self, class_no, sect_name, teacher_id):
-        super().classes[class_no][sect_name].add_teacher(teacher_id)
+        super().classes[class_no][sect_name].add_teacher(teacher_id, self.teachers[teacher_id])  # add teacher name
 
     def add_class(self):
         self.no_classes = self.no_classes + 1
@@ -201,14 +214,20 @@ def admin_loop(admin):
                     break
                 except ValueError:
                     print("Invlalid try again!")
-        if a_ch == "print":
+        elif a_ch == "print":
             admin.print()
+        elif a_ch == "quit":
+            print("Exiting admin control")
+        else:
+            print("Unknown selection")
+
 
 def user_loop(user):
     a_ch = ""
     while a_ch != 'quit':
         a_ch = str(
-            input("search: Search for Student attendance  take: Take attendance for class  print: Print out class and student information"))
+            input(
+                "search: Search for Student attendance  take: Take attendance for class  print: Print out class and student information"))
         if a_ch == "search":
             while True:
                 try:
@@ -223,12 +242,15 @@ def user_loop(user):
             user.take_attendance()
         elif a_ch == "print":
             user.print()
+        elif a_ch == "quit":
+            print("Exiting user control")
         else:
             print("Unknown selection")
 
+
 admin = Admin()
 user = User()
-user.update_attendance() #temp for debugging
+user.update_attendance()  # temp for debugging
 # admin.add_sect(1, "A", 5)
 # create lambdas to add classes and sections
 # admin.add_students("Toaster", 1, 6)
@@ -243,5 +265,8 @@ while (ch != "quit"):
         admin_loop(admin)
     elif ch == "user":
         user_loop(user)
-
+    elif ch == "quit":
+        print("Exiting program")
+    else:
+        print("Unknown selection")
 # user.print()
