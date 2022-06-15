a156ade8e78e90b5e7b92c59606685a80c3faeaa
Rang-Alv <71291523+Rang-Alv@users.noreply.github.com>
2022-06-15 10:32:46 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,126 @@
+class Student:
+    def __init__(self, name, class_name="", id=0):
+        self.name = name
+        self.id = id
+        self.class_name = class_name
+        self.record = []
+
+    def set_class(self, name, class_name):
+        self.class_name = class_name
+
+    def display_details(self):
+        print("Student name:", self.name, " Student ID:", self.id, " Student class:", self.class_name)
+
+
+class Section:
+    def __init__(self, name, teacher_id=0):
+        self.class_name = name
+        self.teacher = teacher_id  # CHANGE to integer ID system
+        self.students = {}  # dict containing attendance record
+
+    def add_teacher(self, teacher_id):
+        self.teacher = teacher_id
+
+    def add_student(self, student_id, attendance=100):
+        self.students[student_id] = attendance  # Add the student with an empty attendance record
+
+    def set_attendance(self):
+        for id in self.students:  # Potensh replace with lambda
+            input_atten = int(input("Attendance for student", id))
+            self.students[id] = input_atten  # Get attendance
+
+    def get_no_students(self):
+        return len(self.students)
+
+    def get_class_name(self):
+        return self.class_name
+
+    def display_details(self):
+        print("Class name:", self.class_name, " Teacher:", self.teacher, " Students:", self.students)
+
+
+class User:
+    students = {}
+    teachers = {}
+    classes = {1: [], 2: [], 3: [], 4: []}
+    no_classes = 4
+
+    def get_student_name(self, id):
+        return self.students[id]
+
+    def get_teacher_name(self, id):
+        return self.teachers[id]
+
+    def print_students(self):
+        for sid in self.students:
+            self.students[sid].display_details()
+
+    def print_classes(self):
+        for l in self.classes:
+            for i in self.classes[l]:
+                i.display_details()
+
+    # Method of writing student attendance
+    def write_attendance(self):
+        while True:
+            input_class = input("Enter class to take attendance:")
+            try:
+                selected_class = self.classes[input_class[0]]
+                break
+            except IndexError:
+                print("Invlalid try again!")
+
+        selected_class.set_attendance()
+
+
+class Admin(User):
+    def __init__(self):
+        teacher_list = [[1, "Mr. Macdonald"], [2, "Ms. Rolex"], [3, "Mr. Airbus"], [4, "Ms. Amadeus"], [5, "Mr. Bose"],
+                        [6, "Ms. Wiley"]]
+        list(filter(lambda teacher_info: self.add_teachers(teacher_info[0], teacher_info[1]), teacher_list))
+
+        sect_list = [[1, "A"], [1, "B"], [1, "C"], [2, "A"], [3, "A"], [3, "B"]]
+        list(filter(lambda sect_info: self.add_sect(sect_info[0], sect_info[1]), sect_list))
+
+        teacher_number = 1
+        for c in super().classes:
+            for n in super().classes[c]:
+                n.add_teacher(teacher_number)
+                teacher_number = teacher_number + 1
+
+    def add_students(self, name, class_no, id):
+        for c in self.classes[class_no]:
+            if c.get_no_students() < 5:  # Find class with space
+                s = Student(name, c.get_class_name(), id)
+                super().students[id] = s
+                c.add_student(id)
+            else:
+                print("No space") # CHANGE make new section in class
+
+    def add_teachers(self, id, name):
+        super().teachers[id] = name
+
+    def add_sect(self, class_no, sect_name):
+        s = Section(str(class_no) + sect_name)
+        super().classes[class_no].append(s)
+
+    def assign_teacher(self, class_no, sect_name, teacher_id):
+        super().classes[class_no][sect_name].add_teacher(teacher_id)
+
+    def add_class(self):
+        self.no_classes = self.no_classes + 1
+        self.classes[self.no_classes] = []
+
+    def print(self):
+        print("Hello Admin")
+        super().print_students()
+        super().print_classes()
+        print(super().teachers)
+
+
+admin = Admin()
+# admin.add_sect(1, "A", 5)
+# create lambdas to add classes and sections
+# admin.add_students("Toaster", 1, 6)
+# Create lambda to add students
+admin.print()
